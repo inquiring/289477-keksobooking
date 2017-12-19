768775ec3dd2077818389888d4267a3e2472acc1
Alexander Suleymanov <evosultanstvo@gmail.com>
2017-12-19 15:57:56 +0300

    Личный проект: максимум подвижности

--- js/map.js
+++ js/map.js
@@ -139,10 +139,48 @@
       setAttributeForm(fieldsetNoticeForm);
     }
 
-    // fillOffers();
-
     mapPinMain.addEventListener('mouseup', onPinMainClick);
   };
 
   loadPage();
+
+  //
+  mapPinMain.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+    // координаты точки, с которой мы начали перемещать pin
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      if (moveEvt.clientY > 100 && moveEvt.clientY < 500) {
+        var shift = {
+          x: startCoords.x - moveEvt.clientX,
+          y: startCoords.y - moveEvt.clientY
+        };
+
+        startCoords = {
+          x: moveEvt.clientX,
+          y: moveEvt.clientY
+        };
+        mapPinMain.style.top = (mapPinMain.offsetTop - shift.y) + 'px';
+        mapPinMain.style.left = (mapPinMain.offsetLeft - shift.x) + 'px';
+      }
+    };
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+      var address = document.querySelector('#address');
+      var addressPoint = ['x: ' + upEvt.clientX, 'y: ' + upEvt.clientY];
+      address.value = addressPoint.join(', ');
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+
+  });
+
+
 })();
