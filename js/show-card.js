1c2df57a84e8beae2ea13f75321959bc78d0be5d
Alexander Suleymanov <evosultanstvo@gmail.com>
2017-12-20 17:15:46 +0300

    Создал модуль show-card.js, экспортирующий в глобальную область видимости функцию showCard

---
+++ js/show-card.js
@@ -0,0 +1,40 @@
+'use strict';
+
+//Функция показывает карточку выбранного жилья по нажатию на метку на карте
+
+window.showCard = (function () {
+  var ESC_KEYCODE = 27;
+  var map = document.querySelector('.map');
+  var mapFiltersContainer = map.querySelector('.map__filters-container');
+  var mapPins = map.querySelector('.map__pins');
+
+  var onPopupClose = function () {
+    var mapCard = document.querySelector('.map__card');
+    var popupClose = mapCard.querySelector('.popup__close');
+    mapCard.classList.add('hidden');
+    window.deactivatePin(mapPins);
+    popupClose.addEventListener('click', onPopupClose);
+  };
+
+  var onKeyEscPress = function (event) {
+    if (event.keyCode === ESC_KEYCODE) {
+      onPopupClose();
+      document.removeEventListener('keydown', onKeyEscPress);
+    }
+  };
+
+  return function (number) {
+
+    var offer = window.renderOffer(window.data.offers[number]);
+
+    map.insertBefore(offer, mapFiltersContainer);
+
+
+    var mapCard = document.querySelector('.map__card');
+    var popupClose = mapCard.querySelector('.popup__close');
+    popupClose.addEventListener('click', onPopupClose);
+    document.addEventListener('keydown', onKeyEscPress);
+  };
+
+
+})();
